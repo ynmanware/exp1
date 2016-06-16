0f208c063844155ace80ad09fa78b3ec49f0e218
Yogesh Manware <yogesh.manware@sungard.com>
2016-06-16 15:09:34 +0530

    r cors

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ var express = require('express'),
  bodyParser = require("body-parser");
  var cron = require('node-schedule');
  
- var cors = require('cors')
+// var db = mongoose.connect('mongodb://localhost/parkingAPI');
 
-
-//  var db = mongoose.connect('mongodb://localhost/parkingAPI');
-
-    var db = mongoose.connect('mongodb://ynmanware:p2ssw0rd@ds028559.mlab.com:28559/parkingonrent');
+   var db = mongoose.connect('mongodb://ynmanware:p2ssw0rd@ds028559.mlab.com:28559/parkingonrent');
 
 var Parking = require('./models/parkingModel');
 
@@ -16,12 +13,17 @@ var Reservation = require('./models/reservationModel');
 
 var app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }))
+
 
 var parkingRouter = require('./routes/parkingRoutes')(Parking);
 var reservationRouter = require('./routes/reservationRoutes')(Reservation, Parking);
